b896875895eb86a4139c1160b3f630e7d9ad2b9a
Bright Annan <brightannan5@gmail.com>
2021-06-02 21:54:16 +0000

    add redux form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addUser, deleteUser } from "./store/usersActions";
+import UsersForm from "./components/UsersForm";
+import UserInfo from "./components/UserInfo";
+
+class App extends Component {
+  addNewUser = (newUser) => {
+    this.props.addUser(newUser);
+  };
+
+  deleteUser = (user_id) => {
+    this.props.deleteUser(user_id);
+  };
+  render() {
+    return (
+      <div className="App">
+        <UsersForm addUser={this.addNewUser} />
+
+        <div className="App_user-info">
+          {this.props.users.map((item) => {
+            return (
+              <UserInfo
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                email={item.email}
+                gen={item.gen}
+                removeUser={this.deleteUser}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  users: state.users,
+});
+
+const mapDispatchToProps = {
+  addUser: addUser,
+  deleteUser: deleteUser,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/store/usersReducer.jsx
@@ -0,0 +1,65 @@
+import { v4 as uuid } from "uuid";
+import { editUser } from "./usersActions";
+
+const initalState = {
+  users: [
+    {
+      id: "1",
+      name: "Ahmed Zaky",
+      email: "ahmed@email.com",
+      gen: 1,
+    },
+    {
+      id: "2",
+      name: "Emmanuel Adebayor",
+      email: "emmanuel@email.com",
+      gen: 4,
+    },
+    {
+      id: "3",
+      name: "Asamoah Gyan",
+      email: "asamoah@email.com",
+      gen: 7,
+    },
+    {
+      id: "4",
+      name: "Stephen Appiah",
+      email: "stephen@email.com",
+      gen: 7,
+    },
+  ],
+};
+
+const usersReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case "ADD_USER":
+      const newUser = {
+        id: uuid(),
+        name: action.payload.name,
+        email: action.payload.email,
+        gen: action.payload.gen,
+      };
+      return { ...state, users: [...state.users, newUser] };
+
+    case "DELETE_USER":
+      const filteredUsers = state.users.filter(
+        (user) => user.id !== action.payload
+      );
+      return { ...state, users: filteredUsers };
+
+    case "EDIT_USER":
+      const editUsers = state.users.map((user) => {
+        if (user.id === action.user_id) {
+          return { ...user, ...action.updated_info };
+        } else {
+          return user;
+        }
+      });
+      return { ...state, users: editUsers };
+
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
