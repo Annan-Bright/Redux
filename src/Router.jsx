e1d5cf5aeb685d138caf8ab98f59b5629f2cf990
Bright Annan <brightannan5@gmail.com>
2021-06-10 10:42:18 +0000

    updated form

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import App from "./App";
-import EditForm from "./components/EidtForm";
+import EditForm from "./components/EditForm";
 
 export default function Router() {
   return (
